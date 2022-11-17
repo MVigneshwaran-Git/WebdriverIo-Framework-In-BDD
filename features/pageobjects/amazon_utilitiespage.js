42784747c97e7b85c52e888ed06e370e69cfdb33
MVigneshwaran-Git <75516302+MVigneshwaran-Git@users.noreply.github.com>
2022-11-17 10:35:32 +0530

    Delete amazon_utilitiespage.js
    optional file

--- features/pageobjects/amazon_utilitiespage.js
+++
@@ -1,8 +0,0 @@
-//const { default: $ } = require("webdriverio/build/commands/browser/$");
-
-class amazon_utilitiespage {
-    get searchBox(){
-        return $('#twotabsearchtextbox')
-    }
-}
-module.exports = new amazon_utilitiespage();
